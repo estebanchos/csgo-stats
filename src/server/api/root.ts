251b250166a586ee9732fa1bd056a4852434fd24
Carlos Ocampo <estebanchos@outlook.com>
2023-08-16 23:15:04 -0400

    create maps router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,8 +1,10 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { playerRouter } from "./routers/player";
+import { mapRouter } from "./routers/map";
 
 export const appRouter = createTRPCRouter({
   players: playerRouter,
+  maps: mapRouter,
 });
 
 export type AppRouter = typeof appRouter;
---
+++ src/server/api/routers/map.ts
@@ -0,0 +1,20 @@
+// import { z } from "zod";
+import {
+  createTRPCRouter,
+  // publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+export const mapRouter = createTRPCRouter({
+  getAll: protectedProcedure
+  .query(({ ctx }) => {
+    return ctx.prisma.maps.findMany({
+      where: {},
+      select: {
+        id: true,
+        name: true,
+        isDefusal: true,
+      }
+    });
+  }),
+});
